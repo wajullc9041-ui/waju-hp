f3a88ac6b5df7fbee86ebd18a6ecefb4b1018745
wajullc9041-ui <waju.llc.9041@gmail.com>
2025-10-04 18:56:23 +0900

    Create env.js

---
+++ env.js
@@ -0,0 +1,8 @@
+// env.js
+// 本番用のSupabase接続情報をここに入れる
+// GitHubには中身を空にしてコミットし、Vercelデプロイ時に置き換えるのが安全です。
+
+window.env = {
+  NEXT_PUBLIC_SUPABASE_URL: "https://htnmjsqgoapvuanrsuma.supabase.co",
+  NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh0bm1qc3Fnb2FwdnVhbnJzdW1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUzMzI3MDEsImV4cCI6MjA3MDkwODcwMX0.1AACTWZmChfmIIfBad0sf-hLV2bnaUt7bVURXnd0uKA"
+};
